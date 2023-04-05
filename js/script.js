dbc056b5ce98a42a9d2b7933c038dee8b27ac672
vcesarini <valeriacesarini@gmail.com>
2023-04-05 15:31:50 -0300

    pruebas varias
    todo

--- js/script.js
+++ js/script.js
@@ -1,101 +1,27 @@
-const movPag=document.querySelector(" .movPag");
-const btn_adelante2 = document.querySelector(".sigPag");
-const btn_atras1=document.querySelector(" .volver-pag1");
-const btn_adelante3 = document.querySelector(".adelante-pag3");
-const btn_adelante4 = document.querySelector(".adelante-pag4");
-const btn_atras2=document.querySelector(" .volver-pag2");
+/* LLAMADA AJAX */
 
-const btn_atras3=document.querySelector(" .volver-pag3");
-const btn_final=document.querySelector(" .fin");
+const HTMLResponse = document.querySelector("#app");
+const templateUL = document.createElement(`ul`);
 
-const progressText= document.querySelectorAll(" .paso p");
-const progressCheck= document.querySelectorAll(" .paso .check");
-const Num= document.querySelectorAll(" .paso .num");
+$.ajax({ method: "GET", url: "http://jsonplaceholder.typicode.com/users" })
+    .done(function (users) {
+        //Mostrar por consola la respuesta de la API
+        console.log(users)
 
-/* max = numero de pagina */
+        //Mostrar respuesta de API en un componente HTML
+        //document.getElementById("response").innerHTML = users;
 
-let max=4;
-let cont=1
-  //botones de avance
-btn_adelante2.addEventListener("click", function(e){
-  
-    e.preventDefault();
-    
-    var nombre=document.getElementById("nombres").value;
+        //Mostrar respuesta de API en un componente HTML recorriendo cada usuario
+        users.forEach((users) => {
+            let elem = document.createElement("li");
+            elem.appendChild(
+                document.createTextNode(`Nombre: ${users.name}, Email: ${users.email}`)
+            );
+            templateUL.appendChild(elem);
+        });
+        HTMLResponse.appendChild(templateUL);
 
-    
-    /* Control de errores */
+    }
+    );
 
-    if(nombre==""){
-document.getElementById("nombres-error").innerHTML="este campo no puede quedar vacio."
-document.getElementById("nombres").style.borderColor="red";
-    }else{
-        document.getElementById("nombres-error").innerHTML=""
-        document.getElementById("nombres").style.borderColor="lightgrey";
-        
-    movPag.style.marginLeft="-25%";
-    /* movimiento de progress bar */ 
-    Num [cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    cont +=1;
-}
-});
-  
-
-
-btn_adelante3.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    movPag.style.marginLeft="-50%"; 
-    Num [cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    cont +=1;
-});
-btn_adelante4.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    movPag.style.marginLeft="-75%"; 
-    Num [cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    cont +=1;
-});
-btn_final.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    Num [cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    cont +=1;
-  alert("aqui finaliza el registro");
-});
-btn_atras1.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    movPag.style.marginLeft="0%"; 
-    Num [cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    cont -=1;
-    
-});
-btn_atras2.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    movPag.style.marginLeft="-25%"; 
-    Num [cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    cont -=1;
-});
-btn_atras3.addEventListener("click", function(e){
-    
-    e.preventDefault();
-    movPag.style.marginLeft="-50%"; 
-    Num [cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    cont -=1;
-});
+/* LLAMADA AJAX */
