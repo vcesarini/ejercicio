46d17a648d63270bd5a9a21d2f5c551fce6cdf73
vcesarini <valeriacesarini@gmail.com>
2023-04-10 11:31:03 -0300

    fake store card

--- js/script.js
+++ js/script.js
@@ -99,3 +99,27 @@ btn_atras3.addEventListener("click", function(e){
     progressText[cont - 2].classList.remove("active");
     cont -=1;
 });
+
+/* FAKE STORE */
+
+const fakeStore = document.querySelector('#store')
+const producto = fetch('https://fakestoreapi.com/products?limit=3');
+
+producto.then(res => res.json())
+        .then(data => {
+            for (const elemento of data) {
+                const nuevoDiv = document.createElement('div');
+                nuevoDiv.classList.add('card');
+                nuevoDiv.innerHTML = `
+                    <img src="${elemento.image}" class="card-img-top" alt="${elemento.title}">
+                    <div class="card-body">
+                      <h5 class="card-title">${elemento.title}</h5>
+                      <p>${elemento.category}</p>
+                      <p class="card-text">${elemento.description}</p>
+                      <p>Precio: $${elemento.price}</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                `;
+                fakeStore.appendChild(nuevoDiv)
+            }
+        });
